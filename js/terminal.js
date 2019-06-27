216ec93eaaa259160d8a15c4900156295bb36d7a
wickstjo <jfwick@gmail.com>
2019-06-27 11:37:42 +0300

    Added passport func

--- js/terminal.js
+++ js/terminal.js
@@ -1,2 +1,18 @@
-// IMPORT TERMINAL MODULE
+// IMPORT HELPER MODULES
 const terminal = require('node-cmd');
+const sha256 = require('sha256');
+
+// GENERATE IDENTIFICATION BASED ON HARDWARE SNAPSHOT & SERIAL NUMBER
+function passport() {
+    return new Promise((resolve, reject) => {
+        terminal.get('sudo lshw', (err, response, stderr) => {
+
+            // HASH THE RESPONSE & RESOLVE
+            resolve(sha256(response));
+        });
+    })
+}
+
+module.exports = {
+    passport
+}
--- run.js
+++ run.js
@@ -1,9 +1,14 @@
 const { init } = require('./js/blockchain.js');
+const { passport } = require('./js/terminal.js');
 
 const { web3, contracts } = init();
 
-contracts.users.events.Action().on('data', event => {
-    console.log('event triggered');
-}).on('error', event => {
-    console.log('error');
+// contracts.users.events.Action().on('data', event => {
+//     console.log('event triggered');
+// }).on('error', event => {
+//     console.log('error');
+// });
+
+passport().then(hash => {
+    console.log(hash);
 });
